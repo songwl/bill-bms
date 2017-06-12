161782a4effde5bfba0899a7318e133b3024474e
yjp2017 <1553897327@qq.com>
2017-06-12 14:37:57 +0800

    inbox，sendbox调整3

--- src/main/java/com/yipeng/bill/bms/dao/inBoxMapper.java
+++ src/main/java/com/yipeng/bill/bms/dao/inBoxMapper.java
@@ -1,4 +1,28 @@
 package com.yipeng.bill.bms.dao;
 
-public class inBoxMapper {
+import com.yipeng.bill.bms.domain.inBox;
+import com.yipeng.bill.bms.domain.noticepublish;
+import com.yipeng.bill.bms.domain.sendBox;
+import com.yipeng.bill.bms.vo.Feedback;
+
+import java.util.List;
+import java.util.Map;
+
+public interface inBoxMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(inBox record);
+
+    int insertSelective(inBox record);
+
+    inBox selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(inBox record);
+
+    int updateByPrimaryKey(inBox record);
+
+    Long selectCount(Map<String ,Object> params);
+
+    List<Feedback> selectInBox(Map<String,Object> params);
+
 }
